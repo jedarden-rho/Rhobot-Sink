a71525383814f1cab0ac94b4cdae8f75f750586a
Jed Arden <jed.arden@rho.co>
2025-01-02 11:09:38 -0500

    another update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,12 @@
 // @ts-check
 import antfu from '@antfu/eslint-config'
-import withNuxt from './.nuxt/eslint.config.mjs'
-
-export default withNuxt(
-  antfu(),
-  {
-    ignores: ['components/ui', '.data', 'public/world.json'],
+export default {
+  ...antfu(),
+  ignores: ['components/ui', '.data', 'public/world.json'],
+  rules: {
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'no-console': 'off',
+    'node/prefer-global/process': 'off',
+    'vue/no-v-html': 'off',
   },
-  {
-    rules: {
-      '@typescript-eslint/ban-ts-comment': 'off',
-      'no-console': 'off',
-      'node/prefer-global/process': 'off',
-      'vue/no-v-html': 'off',
-    },
-  },
-)
+}
